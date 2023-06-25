a2c8abacb83e0f024ec2a74aaa63af51b2f9b1dd
alext <alext@DESKTOP-3IRSC3F>
2023-06-25 16:06:03 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private string currentNumber = string.Empty;
+        private string operation = string.Empty;
+        private double result = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string number = (string)((Button)sender).Content;
+            currentNumber += number;
+            resultTextBox.Text = currentNumber;
+        }
+
+        private void OperatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            string op = (string)((Button)sender).Content;
+            if (!string.IsNullOrEmpty(currentNumber))
+            {
+                if (!string.IsNullOrEmpty(operation))
+                {
+                    Calculate();
+                }
+
+                operation = op;
+                result = double.Parse(currentNumber);
+                currentNumber = string.Empty;
+            }
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            if (!string.IsNullOrEmpty(currentNumber) && !string.IsNullOrEmpty(operation))
+            {
+                double number = double.Parse(currentNumber);
+                switch (operation)
+                {
+                    case "+":
+                        result += number;
+                        break;
+                    case "-":
+                        result -= number;
+                        break;
+                    case "*":
+                        result *= number;
+                        break;
+                    case "/":
+                        result /= number;
+                        break;
+                }
+
+                currentNumber = string.Empty;
+                operation = string.Empty;
+                resultTextBox.Text = result.ToString();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentNumber = string.Empty;
+            operation = string.Empty;
+            result = 0;
+            resultTextBox.Text = "0";
+        }
+    }
+}
